347caf8ef68a0444cf44c1ae12b53e431d577d1d
Your Name <you@example.com>
2015-12-28 20:22:01 +0600

    init

---
+++ trunk/FacebookApp/FacebookApp/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace FacebookApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.facebookService1 = new Facebook.Winforms.Components.FacebookService(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(272, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 97);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // facebookService1
+            // 
+            this.facebookService1.ApplicationKey = null;
+            this.facebookService1.SessionKey = null;
+            this.facebookService1.uid = ((long)(0));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 305);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private Facebook.Winforms.Components.FacebookService facebookService1;
+    }
+}
+
---
+++ trunk/FacebookApp/FacebookFriend/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Facebook.Session;
+using Facebook.Schema;
+
+namespace FacebookFriend
+{
+    public partial class Form1 : Form
+    {
+        DesktopSession ds = new DesktopSession("429267130467465", true);
+        IList<user> friends;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ds.LoginCompleted += new EventHandler<AsyncCompletedEventArgs>(ds_LoginCompleted);
+            ds.LogoutCompleted += new EventHandler<AsyncCompletedEventArgs>(ds_LogoutCompleted);
+            button2.Enabled = false;
+            ds.Login();
+        }
+        void ds_LogoutCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            button2.Enabled = false;
+        }
+
+        void ds_LoginCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+
+            button2.Enabled = true;
+            label1.Text = ds.UserId.ToString();
+            facebookService1.ApplicationKey = ds.ApplicationKey;
+            facebookService1.SessionKey = ds.SessionKey;
+            facebookService1.uid = ds.UserId;
+            facebookService1.ConnectToFacebook();
+            facebookService1.Users.Session.SessionSecret = ds.SessionSecret;
+            friends = facebookService1.Friends.GetUserObjects();
+            int sira = 0;
+            listView1.LargeImageList = ımageList1;
+
+            foreach (user fr in friends)
+            {
+                ımageList1.Images.Add(sira.ToString(), fr.picture);
+                listView1.Items.Add(new ListViewItem(fr.name, sira.ToString()));
+                sira = sira + 1;
+            }
+            //outlookkontaktdoldur();
+        }
+    }
+}
